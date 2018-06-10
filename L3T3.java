b05edadde014ad8b75e96a8e04e774ee8f10f82f
iea86 <40144853+iea86@users.noreply.github.com>
2018-06-10 22:05:30 +0300

    Add files via upload

--- L3T3.java
+++ L3T3.java
@@ -4,31 +4,32 @@ import java.util.Scanner;
 
 public class L3T3 {
 	public static void main (String[] args) {
-		int a = 0, b = 360, r = 0;
+		int a = 0, b = 360, r = 0, h = 1;
 		double f = 0;
 		Scanner x = new Scanner(System.in);
 		
-		System.out.print("Enter x (should be > 0):");
+		System.out.print("Enter x:");
 		
 		
 		if (x.hasNextInt()) {
 			r = x.nextInt();
 			if (r > a) {
 				System.out.print("-------------------------\n");
-				System.out.print("x       f\n");
+				System.out.print("    x       f\n");
 				System.out.print("-------------------------\n");
             
-				for (int i = r; i < b+1; i++) {
+				for (r=1; r>=a && r<=b; r=r + h) {
 				
-					f = 2 * Math.tan(i/2) +1;
-					System.out.print(i  + "      "+f+"\n");
+					f = 2 * Math.tan(r/2) +1;
+					System.out.print("    " + r  + "      "+f+"\n");
 					
 				}
-			}
-			
+			}			
 			
 		}
 				
      }
 	
 }
+
+
